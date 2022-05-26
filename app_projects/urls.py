cf5808ab2ae08a10a8ba36135f4bc4d99d9367fc
kotlyarevskiy.v <48545383+kotlyarevskiy@users.noreply.github.com>
2022-05-26 17:56:43 +0300

    copy command for project

--- app_projects/urls.py
+++ app_projects/urls.py
@@ -4,7 +4,8 @@ from .views import *
 urlpatterns = [
     path('',                        projects,       name = 'projects'),
     path('project',                 project_page,   name = 'project_page'),
-    path('project/<int:id>',        project_page,   name = 'project_update'),
+    path('project/<int:id>/update', project_page,   name = 'project_update'),
+    path('project/<int:id>/copy',   project_copy,   name = 'project_copy'),
     path('project/<int:id>/delete', project_delete, name = 'project_delete'),
 
 ]
--- app_projects/views.py
+++ app_projects/views.py
@@ -24,6 +24,8 @@ def project_delete(request, id):
     
     if request.method == 'GET':
         
+        # Delete
+        
         # TODO: add a question!!!!!!
         # TODO: add messages.
         
@@ -34,17 +36,16 @@ def project_delete(request, id):
     return redirect('projects')
 
 @permission_required(['app_projects.add_projects', 'app_projects.change_projects'], login_url='login_user')
-def project_page(request, id=0):
+def project_copy(request, id=0):
     
-    if id:
-       
-        project = Projects.objects.get(id=id)
-    
-    else:
-       
-        project = None   
+    project = Projects.objects.get(id=id) if id else None
+    action = _('copy')    
     
     if request.method == 'POST':
+        
+        # Edit
+        
+        action = _('edit')
         data = request.POST
         project_form = ProjectForm(data, instance=project)
         
@@ -53,9 +54,19 @@ def project_page(request, id=0):
             project_form.save()
             messages.success(request, _('The changes has been saved.'))
             
-    else:
+    elif request.method == 'GET':        
         
-        project_form = ProjectForm(instance=project)
+        # Create if None or Copy
+                
+        if not project == None:
+            
+            action = _('copy')
+            project.pk = None
+            project.save()
+        
+        
+    project_form = ProjectForm(instance=project)
+        
     
     static_css_list = []
     static_js_list = ['js/project_page.js']   
@@ -63,6 +74,50 @@ def project_page(request, id=0):
     context = { 'title':            _('Project page'), 
                 'content_title':    _('Project page'),
                 'active':           'projects',
+                'action':           action,
+                'project_form':     project_form, 
+                'static_files':     {'css': static_css_list, 'js': static_js_list},
+                }
+                     
+    return render(request, 'projects/project_page.html', context)
+
+@permission_required(['app_projects.add_projects', 'app_projects.change_projects'], login_url='login_user')
+def project_page(request, id=0):
+    
+    project = Projects.objects.get(id=id) if id else None
+    action = _('create')    
+    
+    if request.method == 'POST':
+        
+        # Edit
+        
+        action = _('edit')
+        data = request.POST
+        project_form = ProjectForm(data, instance=project)
+        
+    
+        if project_form.is_valid():
+            project_form.save()
+            messages.success(request, _('The changes has been saved.'))
+            
+    elif request.method == 'GET':        
+        
+        # Create and Edit
+                
+        if not project == None:
+            
+            action = _('edit')
+        
+        project_form = ProjectForm(instance=project)
+        
+    
+    static_css_list = []
+    static_js_list = ['js/project_page.js']   
+             
+    context = { 'title':            _('Project page'), 
+                'content_title':    _('Project page'),
+                'active':           'projects',
+                'action':           action,
                 'project_form':     project_form, 
                 'static_files':     {'css': static_css_list, 'js': static_js_list},
                 }
