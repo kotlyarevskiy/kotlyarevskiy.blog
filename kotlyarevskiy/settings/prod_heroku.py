0e0214749c5cbf725e666002a2b855fcadcdcfd5
kotlyarevskiy.v <48545383+kotlyarevskiy@users.noreply.github.com>
2022-06-03 00:19:51 +0300

    add heroku settings to production

--- kotlyarevskiy/settings/prod_heroku.py
+++ kotlyarevskiy/settings/prod_heroku.py
@@ -12,3 +12,33 @@ DEBUG = False
 ALLOWED_HOSTS = [
                  'kotlyarevskiy-blog.heroku.com',
                  'kotlyarevskiy.blog',]
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'MYAPP': {
+            'handlers':['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+django_on_heroku.settings(locals(), staticfiles=False)
+del DATABASES['default']['OPTIONS']['sslmode']
