2267f786a00964f632036ae3a5c94a92a5ad3ee2
kotlyarevskiy.v <48545383+kotlyarevskiy@users.noreply.github.com>
2022-06-05 23:18:59 +0300

    projects fix

--- kotlyarevskiy/settings/prod_heroku.py
+++ kotlyarevskiy/settings/prod_heroku.py
@@ -10,25 +10,14 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-                 'kotlyarevskiy-blog.herokuapp.com',
+                 'kotlyarevskiy-blog.heroku.com',
                  'kotlyarevskiy.blog',]
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_URL = '_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
