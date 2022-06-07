d577c1adf17a55226847444ba6190d5bc46e6216
kotlyarevskiy.v <48545383+kotlyarevskiy@users.noreply.github.com>
2022-06-07 21:57:50 +0300

    translations

--- kotlyarevskiy/settings/prod_heroku.py
+++ kotlyarevskiy/settings/prod_heroku.py
@@ -13,9 +13,15 @@ ALLOWED_HOSTS = [
                  'kotlyarevskiy-blog.heroku.com',
                  'kotlyarevskiy.blog',]
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '_static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
